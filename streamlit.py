a70bd0f6ef63dab61a9b85cd8d3b081501790809
Lextor-lb <seriousglobin@gmail.com>
2024-12-10 16:32:50 +0630

    fix chatbot

--- streamlit.py
+++ streamlit.py
@@ -14,8 +14,8 @@ st.set_page_config(
 )
 
 
-def main():
 
+def main():
     page = st.sidebar.radio(
         "Select a Page", ("Plant Health Prediction", "Chatbot"))
 
@@ -25,17 +25,21 @@ def main():
         chatbot_page()
 
     st.sidebar.markdown("##### Data")
+    
+    # Create an empty container to hold the plant data in the sidebar
+    plant_data_container = st.sidebar.empty()
+
     if "plant_data" in st.session_state:
-        st.sidebar.write(st.session_state.plant_data)
-        
+        # Display the plant data within the container
+        plant_data_container.write(st.session_state.plant_data)
+
+        # Display the "Clear Data" button, and remove the plant data when clicked
         if st.sidebar.button("Clear Data"):
-                del st.session_state.plant_data
-                st.rerun() 
+            del st.session_state.plant_data
+            plant_data_container.empty()  # Clear the container where data was displayed
+            st.rerun()
     else:
-        st.sidebar.error("Data haven't choosen")
-        
-        
-       
+        st.sidebar.error("Data haven't been chosen")
 
 
 # Initialize the session state for the first time
@@ -44,3 +48,25 @@ if "page" not in st.session_state:
 
 if __name__ == "__main__":
     main()
+
+
+# def main():
+
+#     page = st.sidebar.radio(
+#         "Select a Page", ("Plant Health Prediction", "Chatbot"))
+
+#     if page == "Plant Health Prediction":
+#         plant_health_page()
+#     elif page == "Chatbot":
+#         chatbot_page()
+
+#     st.sidebar.markdown("##### Data")
+#     if "plant_data" in st.session_state:
+#         st.sidebar.write(st.session_state.plant_data)
+        
+#         if st.sidebar.button("Clear Data"):
+#                 del st.session_state.plant_data
+#                 plant_data_container.empty()
+#     else:
+#         st.sidebar.error("Data haven't choosen")
+        
